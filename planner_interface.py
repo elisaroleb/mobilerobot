387e4046ca12a5b7126e62df57a930c6ffdff739
elisaroleb <elisaroleb@gmail.com>
2025-11-13 15:40:18 +0100

    Update planner_interface.py

--- planner_interface.py
+++ planner_interface.py
@@ -3,7 +3,7 @@ import os
 
 def call_planner(domain_file="domain.pddl", problem_file="problem.pddl", output_file="plan.txt"):
     """
-    Ejecuta Fast Downward y guarda el plan resultante.
+    Executes Fast Donward and saves the resutlting plan. 
     """
     try:
         cmd = [
@@ -18,15 +18,15 @@ def call_planner(domain_file="domain.pddl", problem_file="problem.pddl", output_
         result = subprocess.run(cmd, capture_output=True, text=True, timeout=60)
 
         if "Solution found" in result.stdout:
-            print("Plan encontrado exitosamente.")
+            print("Plan succesufully found.")
             if os.path.exists("sas_plan"):
                 os.rename("sas_plan", output_file)
             return True
         else:
-            print("No se encontró plan.")
+            print("No plan founded.")
             print(result.stdout)
             return False
 
     except Exception as e:
-        print(f"Error ejecutando el planificador: {e}")
+        print(f"Error executing the planifier: {e}")
         return False
